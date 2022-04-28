a4612784fe3aced92b40d8051bd152e570ecd12e
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-28 10:24:48 -0500

    employee commit

---
+++ Employee List/src/EmployeeName.java
@@ -0,0 +1,11 @@
+
+public class EmployeeName {
+
+	public static void main(String[] args) {
+		System.out.println("john, marketing manager");
+		System.out.println("mark, marketing manager");
+		System.out.println("angela, marketing manager");
+		System.out.println("Tom, marketing manager");
+	}
+
+}
